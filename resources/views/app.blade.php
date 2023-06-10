2a66c47b4c5341c351549472743e38146454b3bd
Cesar Martinez <cesarmartinez9311@gmail.com>
2023-06-09 22:12:16 -0600

    init react

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LARAVEL REACT SPA</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
 </head>
 <body>
 <div id="app"></div>
-<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
