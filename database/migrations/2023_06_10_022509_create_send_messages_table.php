d19f17c235182a31d17544d9d7f58f608b295928
Cesar Martinez <cesarmartinez9311@gmail.com>
2023-06-09 21:38:04 -0600

    add migration

---
+++ database/migrations/2023_06_10_022509_create_send_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('send_messages', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('success');
+            $table->string('confirmation');
+            $table->foreignId('message_id')->constrained('messages');
+            $table->dateTime('date_sent')->nullable();
+            $table->string('error_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('send_messages');
+    }
+};
