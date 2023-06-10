fc11862178bc546c218504806e9fae8ffb2613b0
Cesar Martinez <cesarmartinez9311@gmail.com>
2023-06-09 21:31:58 -0600

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(MessageController::class)->group(function () {
+    Route::get('/messages', 'index');
+    Route::post('/messages', 'store');
+});
+
+Route::get('/id', [GuestController::class,'index']);
+
+
